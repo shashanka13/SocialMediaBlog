8e5afcbb514f16bb1bf8dc11cd7a385158757332
shashanka13 <163371256+shashanka13@users.noreply.github.com>
2024-12-17 14:38:06 -0600

    Converted into Spring Boot App with Initial setup Files and Required Dependencies into Pom.xml

---
+++ src/test/java/com/JavaProjects/socialmediablog/SocialMediaBlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JavaProjects.socialmediablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaBlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
